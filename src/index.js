67d492acb99179fc359cf4b35c7d6e0158110b7e
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 11:08:36 -0400

    Install thunk for async requests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import rootReducer from "./reducers/rootReducer";
 
 import App from "./App";
 
-const store = createStore(rootReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <Provider store={store}>
