1ba89a144b0e12a773721e7f8333361215690044
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 11:59:05 -0400

    UserCreate is a controlled form

--- src/components/UserCreate.js
+++ src/components/UserCreate.js
@@ -6,13 +6,35 @@ class UserCreate extends React.Component {
     password: "",
   };
 
+  onChangeInput = (event) =>
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+
+  onSubmitForm = (event) => {
+    event.preventDefault();
+    console.log(this.state);
+  };
+
   render() {
     return (
-      <form>
-        <label>Username</label>
-        <input type='text' placeholder='Username...' />
-        <label>Password</label>
-        <input type='password' placeholder='Password...' />
+      <form onSubmit={this.onSubmitForm}>
+        <label htmlFor='username'>Username</label>
+        <input
+          onChange={this.onChangeInput}
+          id='username'
+          name='username'
+          type='text'
+          placeholder='Username...'
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          onChange={this.onChangeInput}
+          id='password'
+          name='password'
+          type='password'
+          placeholder='Password...'
+        />
         <button type='submit'>Create User</button>
       </form>
     );
