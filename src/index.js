05e224a5d8c7148d2d8fdaff37a585a2e2d42743
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-09 11:06:46 -0400

    create rootReducer, wrap App with Provider, create initial store

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import rootReducer from "./reducers/rootReducer";
 
 import App from "./App";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import usersReducer from "./usersReducer";
+
+// can rename how data is displayed in the store here
+// by providing a key to refer to the reducer
+export default combineReducers({
+  users: usersReducer,
+});
