3dc6a0a9ae929950165dca1a66cb09f6d6a778b9
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-11 14:16:52 -0400

    style the login form and add a NavLink to the signup page

--- src/components/users/UserLogin.js
+++ src/components/users/UserLogin.js
@@ -2,7 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { loginUser } from "../../actions/usersActions";
-import { Redirect } from "react-router-dom";
+import { Redirect, NavLink } from "react-router-dom";
 
 class UserCreate extends React.Component {
   state = {
@@ -23,33 +23,70 @@ class UserCreate extends React.Component {
   render() {
     if (!this.props.currentUser) {
       return (
-        <form className='ui form' onSubmit={this.onSubmitForm}>
-          <div className='field'>
-            <label htmlFor='username'>Username</label>
-            <input
-              onChange={this.onChangeInput}
-              id='username'
-              name='username'
-              type='text'
-              placeholder='Username...'
-            />
+        <div className='ui placeholder segment'>
+          <div className='ui two column very relaxed stackable grid'>
+            <div className='column'>
+              <form onSubmit={this.onSubmitForm} className='ui form'>
+                <div className='field'>
+                  <label htmlFor='username'>Username</label>
+                  <div className='ui left icon input'>
+                    <input
+                      onChange={this.onChangeInput}
+                      id='username'
+                      name='username'
+                      type='text'
+                      placeholder='Username...'
+                    />
+                    <i className='user icon'></i>
+                  </div>
+                </div>
+                <div className='field'>
+                  <label htmlFor='password'>Password</label>
+                  <div className='ui left icon input'>
+                    <input
+                      onChange={this.onChangeInput}
+                      id='password'
+                      name='password'
+                      type='password'
+                      placeholder='Password...'
+                    />
+                    <i className='lock icon'></i>
+                  </div>
+                </div>
+                <button type='submit' className='ui blue submit button'>
+                  Log In
+                </button>
+              </form>
+            </div>
+            <div className='middle aligned column'>
+              <NavLink to='/users/new' className='ui big button'>
+                <i className='signup icon'></i>
+                Sign Up
+              </NavLink>
+            </div>
           </div>
-          <div className='field'>
-            <label htmlFor='password'>Password</label>
-            <input
-              onChange={this.onChangeInput}
-              id='password'
-              name='password'
-              type='password'
-              placeholder='Password...'
-            />
-          </div>
-          <button className='ui green button' type='submit'>
-            Create User
-          </button>
-        </form>
+          <div className='ui vertical divider'>Or</div>
+        </div>
       );
     }
+
+    // if (!this.props.currentUser) {
+    //   return (
+    //     <form className='ui form' >
+    //       <div className='field'>
+    //         <label htmlFor='username'>Username</label>
+
+    //       </div>
+    //       <div className='field'>
+    //         <label htmlFor='password'>Password</label>
+
+    //       </div>
+    //       <button className='ui green button' type='submit'>
+    //         Create User
+    //       </button>
+    //     </form>
+    //   );
+    // }
 
     return <Redirect to='/' />;
   }
