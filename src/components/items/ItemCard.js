4b553a82042e3bc57037cab89ba040b69b223467
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-06-12 10:46:22 -0400

    add toggle to update packed status of items

--- src/components/items/ItemCard.js
+++ src/components/items/ItemCard.js
@@ -1,6 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Item = ({ item }) => {
+const Item = ({ item, user }) => {
+  const onChangeToggle = () => {
+    console.log(user);
+    console.log(!item.attributes.packed);
+  };
   return (
     <div className='ui card'>
       <div className='content'>
@@ -11,8 +16,23 @@ const Item = ({ item }) => {
         <i className='box icon' />
         {item.attributes.packed ? "Packed" : "Still needs to be packed"}
       </div>
+      <div className='content'>
+        <div className='ui toggle checkbox'>
+          <input
+            onChange={onChangeToggle}
+            type='checkbox'
+            name='packed'
+            value={item.packed}
+          />
+          <label>Already Packed?</label>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Item;
+const mapStateToProps = ({ usersData }) => ({
+  user: usersData.currentUser.data,
+});
+
+export default connect(mapStateToProps)(Item);
